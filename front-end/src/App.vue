763a78699b43204c8c101dc41f6185120254fa5e
mark <yw228@byu.edu>
2021-04-10 12:52:03 -0600

    part 2 done
    p2 done

---
+++ front-end/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <header id="header">
+      <h1>Photo Bomb</h1>
+      <nav>
+        <router-link to="/"><i class="fas fa-home"></i></router-link>
+        <router-link to="/dashboard"><i class="fas fa-user"></i></router-link>
+      </nav>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<style>
+/* Color scheme: https://paletton.com/#uid=7040u0knHs+edG7jrvYscpiuCk2 */
+/* red: #e74c3c
+ * blue: #277E8E
+ */
+body {
+  font-family: 'Work Sans', sans-serif;
+  font-weight: 300;
+  font-size: 13pt;
+  margin: 0px 200px;
+}
+
+#header {
+  /* Semi-circle */
+  margin: 0 1em 1em 0;
+  height: 100px;
+  width: 200px;
+  border-bottom-left-radius: 200px;
+  border-bottom-right-radius: 200px;
+  /* Fixed position */
+  position: fixed;
+  z-index: 10000;
+  left: 50%;
+  transform: translate(-50%, 0);
+  /* Color and alignment */
+  background: #e74c3c;
+  text-align: center;
+  box-shadow: 0 0 0 1em #FFF;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+}
+
+h1 {
+  color: #fff;
+  font-size: 18px;
+}
+
+h2 {
+  font-size: 12px;
+}
+
+#header .fas {
+  font-size: 25px;
+  color: #fff;
+  width: 50px;
+}
+
+.pure-button-primary {
+  background-color: #277E8E;
+}
+</style>
